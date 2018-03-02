a0b1661f9025e6e284e49893bb28d42669f500c5
Gursimran Singh <gursimransingh.gsh@gmail.com>
2018-03-02 21:11:48 +0530

    part 13/42 -- complete

--- src/newsletter/views.py
+++ src/newsletter/views.py
@@ -3,4 +3,11 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-	return render(request, "home.html", {})
+	title = 'Welcome'
+	if request.user.is_authenticated:
+		title = 'Hello {:s}'.format(str(request.user))
+
+	context = {
+		'title': title
+	}
+	return render(request, "home.html", context)
